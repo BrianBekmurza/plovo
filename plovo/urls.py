01a62f7983803c1625c850587c75a17ad7ce2dcd
Asylbek Bekmurzaev <bekmurzaev.asylbek@gmail.com>
2021-05-02 21:43:21 +0600

    added order app

--- plovo/urls.py
+++ plovo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('dish/', include('dish.urls', namespace='dish'))
+    path('dish/', include('dish.urls', namespace='dish')),
+    path('order/', include('order.urls', namespace='order'))
 ]
