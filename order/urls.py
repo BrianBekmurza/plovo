5e6396793f342d19307b7e7f62f69e7b1f512016
Asylbek Bekmurzaev <bekmurzaev.asylbek@gmail.com>
2021-05-02 23:11:49 +0600

    added two functions in one CBV, OrderListCreateView and OrderView

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import OrderListCreateView, OrderView
 
 app_name = 'order'
 
 urlpatterns = [
-   
+   path('', OrderListCreateView.as_view(), name='order-list-create'),
+   path('<int:pk>/', OrderView.as_view(), name='order')
 ]
