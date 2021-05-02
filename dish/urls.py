936a264b9682cb8dc7e709f85794b8db186a699c
Asylbek Bekmurzaev <bekmurzaev.asylbek@gmail.com>
2021-05-02 20:59:46 +0600

    added dish-update and dish-create API

--- dish/urls.py
+++ dish/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import DishListAPIView
+from .views import DishListAPIView, DishDetailAPIView, DishUpdateAPIView, DishDeleteAPIView, DishCreateAPIView 
 
 app_name = 'dish'
 
 urlpatterns = [
     path('dish/', DishListAPIView.as_view(), name='dishes'),
+    path('<int:pk>/', DishDetailAPIView.as_view(), name='dish'),
+    path('<int:pk>/update/', DishUpdateAPIView.as_view(), name='update-dish'),
+    path('<int:pk>/delete/', DishDeleteAPIView.as_view(), name='delete-dish'),
+    path('create/', DishCreateAPIView.as_view(), name='create-dish'),
 ]
